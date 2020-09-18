2a80e3163ec00eb3a7ea889f73b84f151e32b946
loglog3 <loglog354@gmail.com>
2020-09-18 17:30:29 +0900

    Connect Backend and Do refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
-import { auth } from "firebase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState();
+
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService().onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
+        setUserObj(user);
       }
       setInit(true);
     });
-  }, []);
-
+  });
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
-      <footer> @copy; Nwitter {new Date().getFullYear()} </footer>
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
-
 export default App;
